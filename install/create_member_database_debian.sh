4a1ee59adf4f53637b4ec8d9ab29e6ed28da10a1
Ian Bowden <woodscooter@gmail.com>
2023-01-02 14:45:08 +0000

    Revised install process

--- install/create_member_database_debian.sh
+++ install/create_member_database_debian.sh
@@ -1,27 +1,37 @@
-sudo setup-lamp-server.sh
+#!/bin/bash
+# sudo setup-lamp-server.sh
 
-sudo mysql_secure_installation
+sudo mysql_secure_installation << EOF
 
-sudo mysql
+n
+n
+y
+y
+y
+y
+EOF
+
+
+sudo mysql << EOF
+
 create user rml identified by 'abc123';
-create database members
+create database members;
 grant all privileges on members.* to 'rml';
 quit
+EOF
 
-cd 	(takes you to your home directory)
+cd      #(takes you to your home directory)
 git clone https://github.com/RichmondMakerLabs/member_database
 cd ~/member_database/install/
 ## make certain that bin/loadsql has execute permissions
 chmod +x ../bin/loadsql
 ## edit bin/loadsql to contain the mysql password instead of the placeholder \<password\>
 ## if the mysql user (line 20 above) is not rml, then edit bin/loadsql after the option -u
-../bin/loadsql members.sql	(creates the tables and loads initial data)
+../bin/loadsql members.sql      #(creates the tables and loads initial data)
 
-sudo mkdir /var/www/auth		(prepare a secure location for password)
+sudo mkdir /var/www/auth                #(prepare a secure location for password)
 cd ~/member_database/php/
-sudo cp *.* /var/www/html	(copies the php files to DocumentRoot)
-sudo mv /var/www/html/rml.inc /var/www/auth/ (move the password file)
+sudo cp *.* /var/www/html       #(copies the php files to DocumentRoot)
+sudo mv /var/www/html/rml.inc /var/www/auth/ #(move the password file)
 sudo ln -s /var/www/html/rml.php /var/www/html/index.php
 sudo rm /var/www/html/index.html
-
-
